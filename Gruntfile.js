970b18874fd914c9a0cc1ddd5ebe8ea43de94b79
Jason Mulligan <jason.mulligan@avoidwork.com>
2019-02-03 14:16:47 -0500

    Adding an eslint build step

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+		eslint: {
+			target: [
+				"Gruntfile.js",
+				"www/assets/js/*.js"
+			]
+		}
+	});
+
+	// tasks
+	grunt.loadNpmTasks("grunt-eslint");
+
+	// aliases
+	grunt.registerTask("test", ["eslint"]);
+	grunt.registerTask("default", ["test"]);
+};
