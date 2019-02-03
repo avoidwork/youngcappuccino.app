b2e2d927b8a7e95b49178d68d07cd244112b503b
Jason Mulligan <jason.mulligan@avoidwork.com>
2019-02-03 01:57:43 -0500

    Adding try/catch to router handlers that could fail

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -1,6 +1,14 @@
 (async function (document, render, fetch, navigator) {
 	const api = "https://api.youngcappuccino.app/api";
 
+	function log (arg, target = "log") {
+		console[target] = arg instanceof Object ? arg : `[app] ${arg}, timestamp=${new Date().toISOString()}`;
+	}
+
+	function error (arg) {
+		return arg.message || arg.stack || arg;
+	}
+
 	function icons (icon, n = 1, nth = 5) {
 		const result = [];
 		let i = -1,
@@ -31,47 +39,64 @@
 
 	async function geoByIP () {
 		const url = new URL(`${api}/geo`);
+		let result;
 
 		url.searchParams.append("format", "application/json");
 
-		const res = await fetch(url.href, {
-			method: "GET",
-			mode: "cors",
-			headers: {
-				accept: "application/json"
-			}
-		}),
-		data = await res.json();
+		try {
+			const res = await fetch(url.href, {
+					method: "GET",
+					mode: "cors",
+					headers: {
+						accept: "application/json"
+					}
+				}),
+				data = await res.json();
 
-		return data.data;
+			result = data.data;
+		} catch (err) {
+			result = null;
+			log(`type=error, source=geoByIP, error="${error(err)}"`, "warn");
+		}
+
+		return result;
 	}
 
 	async function search (lat, long) {
 		const url = new URL(`${api}/search`);
+		let result;
 
 		url.searchParams.append("format", "application/json");
 		url.searchParams.append("lat", lat);
 		url.searchParams.append("long", long);
 
-		const res = await fetch(url.href, {
-				method: "GET",
-				mode: "cors",
-				headers: {
-					accept: "application/json"
-				}
-			}),
-			data = await res.json();
+		try {
+			const res = await fetch(url.href, {
+					method: "GET",
+					mode: "cors",
+					headers: {
+						accept: "application/json"
+					}
+				}),
+				data = await res.json();
 
-		return data.data;
+			result = data.data;
+		} catch (err) {
+			result = [];
+			log(`type=error, source=search, error="${error(err)}"`, "warn");
+		}
+
+		return result;
 	}
 
 	async function display (arg) {
 		if (arg !== null) {
 			const $list = document.querySelector("#list"),
-				results = await search(arg.location.latitude, arg.location.longitude);
+				results = await search(arg.location.latitude, arg.location.longitude),
+				valid = results instanceof Array && results.length > 0;
 
 			render(() => {
-				if (results === null || (results instanceof Array && results.length === 0)) {
+				if (valid === false) {
 					$list.innerText = "Can't find an open cappuccino shop.";
 				} else {
 					results.length = 2;
