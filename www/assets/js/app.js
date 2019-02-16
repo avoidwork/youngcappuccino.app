b98d6c6767424ea0054a6f0a2587322895311987
Jason Mulligan <jason.mulligan@avoidwork.com>
2019-02-16 11:16:39 -0500

    Adding bulma columns

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -44,11 +44,13 @@
 		log(`type=card, id=${id}, name="${name}"`);
 
 		return `
-<div class="store">
-	<div class="title is-size-3">${name}</div>
-	<div class="subtitle is-size-5">${address}</div>
-	<div>${icons("star", rating, 5)}</div>
-	<div>${icons("dollar-sign", price, 5)}</div>
+<div class="column">
+	<div class="store">
+		<div class="title is-size-3">${name}</div>
+		<div class="subtitle is-size-5">${address}</div>
+		<div>${icons("star", rating, 5)}</div>
+		<div>${icons("dollar-sign", price, 5)}</div>
+	</div>
 </div>
 `;
 	}
