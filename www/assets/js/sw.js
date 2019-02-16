7864a77bc837cbf498933484dd95b523d622ba07
Jason Mulligan <jason.mulligan@avoidwork.com>
2019-02-16 00:41:02 -0500

    Tweaking service worker

--- www/assets/js/sw.js
+++ www/assets/js/sw.js
@@ -6,29 +6,24 @@ const name = "young-cappuccino-cache-v1",
 		"/assets/css/style.css.map",
 		"/assets/css/style.scss",
 		"/assets/css/font-awesome/css/all.css",
+		"/assets/css/font-awesome/webfonts/fa-solid-900.woff2",
 		"/assets/js/app.js",
 		"/assets/img/logo.svg"
 	];
 
 self.addEventListener("install", ev => ev.waitUntil(caches.open(name).then(cache => cache.addAll(urls))));
 
-self.addEventListener("fetch", ev => ev.respondWith(caches.match(ev.request).then(res => {
-	if (res) {
-		return res;
+self.addEventListener("fetch", ev => ev.respondWith(caches.match(ev.request).then(res => res ? res : fetch(ev.request).then(response => {
+	let result;
+
+	if (!response || response.status !== 200 || response.type !== "basic") {
+		result = response;
+	} else {
+		const responseToCache = response.clone();
+
+		caches.open(name).then(cache => cache.put(ev.request, responseToCache));
+		result = response;
 	}
 
-	return fetch(ev.request).then(response => {
-		let result;
-
-		if (!response || response.status !== 200 || response.type !== "basic") {
-			result = response;
-		} else {
-			const responseToCache = response.clone();
-
-			caches.open(name).then(cache => cache.put(ev.request, responseToCache));
-			result = response;
-		}
-
-		return result;
-	});
-})));
+	return result;
+}))));
