c62d8101772c7665c1004bb464f398b7eb1cc8ea
Jason Mulligan <jason.mulligan@avoidwork.com>
2019-10-24 19:48:04 -0400

    Updating service worker, updating dependencies

--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,5 @@
-const name = "young-cappuccino-cache-v1",
+const name = "young-cappuccino-cache-v2",
+	timeout = 18e2, // 30 min
 	urls = [
 		"/",
 		"/assets/css/bulma.css",
@@ -10,21 +11,49 @@ const name = "young-cappuccino-cache-v1",
 		"/assets/js/app.js",
 		"/assets/img/logo.svg",
 		"/assets/img/fav_icon.png"
-	];
+	],
+	cacheable = (arg = '') => (arg.includes('no-cache') || arg.includes('no-store') || arg.includes('max-age=0')) === false;
 
-self.addEventListener("install", ev => ev.waitUntil(caches.open(name).then(cache => cache.addAll(urls))));
+self.addEventListener('activate', ev => ev.waitUntil(caches.keys().then(args => {
+	const x = args.filter(i => i !== name);
 
-self.addEventListener("fetch", ev => ev.respondWith(caches.match(ev.request).then(res => res ? res : fetch(ev.request).then(response => {
-	let result;
+	return x.length === 0 ? Promise.resolve() : Promise.all(x.map(i => caches.delete(i)));
+})));
 
-	if (!response || response.status !== 200 || response.type !== "basic") {
-		result = response;
-	} else {
-		const responseToCache = response.clone();
+self.addEventListener('install', ev => {
+	self.skipWaiting();
 
-		caches.open(name).then(cache => cache.put(ev.request, responseToCache));
-		result = response;
+	return ev.waitUntil(caches.open(name).then(cache => cache.addAll(urls)).catch(() => void 0));
+});
+
+self.addEventListener('fetch', ev => ev.respondWith(new Promise(async resolve => {
+	const cache = await caches.open(name),
+		cached = await cache.match(ev.request);
+	let res;
+
+	try {
+		if (ev.request.method === 'GET') {
+			const now = new Date().getTime();
+
+			if (cached !== void 0 && new Date(cached.headers.get('date')).getTime() + Number((cached.headers.get('cache-control') || '').replace(/[^\d]/g, '') || timeout) * 1e3 > now) {
+				res = cached.clone();
+			} else {
+				res = await fetch(ev.request);
+
+				if (res.status === 200 && res.type === 'basic' && cacheable(res.headers.get('cache-control'))) {
+					await cache.put(ev.request, res.clone());
+				}
+			}
+		} else {
+			res = await fetch(ev.request);
+
+			if (ev.request.method !== 'HEAD' && ev.request.method !== 'OPTIONS' && res.status < 400 && res.type === 'basic') {
+				await cache.delete(ev.request);
+			}
+		}
+	} catch (err) {
+		void 0;
 	}
 
-	return result;
-}))));
+	resolve(res);
+})));
