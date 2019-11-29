29958aafa239ed2c388fec45cd4f9bd97691dec3
Jason Mulligan <jason.mulligan@avoidwork.com>
2019-11-28 19:56:58 -0500

    Undoing audit stuff

--- www/sw.js
+++ www/sw.js
@@ -1,7 +1,8 @@
-const name = "young-cappuccino-cache-v7",
+const name = "young-cappuccino-cache-v8",
 	timeout = 18e2, // 30 min
 	urls = [
 		"/",
+		"/manifest.json",
 		"/assets/css/bulma.css",
 		"/assets/css/style.css",
 		"/assets/css/style.css.map",
