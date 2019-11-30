51de3a9b384b466091a95d498435f63d16456d85
Jason Mulligan <jason.mulligan@avoidwork.com>
2019-11-30 10:58:39 -0500

    Removing unneeded assets, generating `sw.js` via `tiny-serviceworker` (`sw --name="ycap" --directories="assets/css,assets/img,assets/js"`)

--- www/sw.js
+++ www/sw.js
@@ -1,43 +1,67 @@
-const name = "young-cappuccino-cache-v8",
-	timeout = 18e2, // 30 min
-	urls = [
-		"/",
-		"/manifest.json",
-		"/assets/css/bulma.css",
-		"/assets/css/style.css",
-		"/assets/css/style.css.map",
-		"/assets/css/style.scss",
-		"/assets/css/font-awesome/css/all.css",
-		"/assets/css/font-awesome/webfonts/fa-solid-900.woff2",
-		"/assets/js/app.js",
-		"/assets/img/logo.svg",
-		"/assets/img/fav_icon.png",
-		"/assets/img/icon_192.png",
-		"/assets/img/icon_512.png"
-	],
-	cacheable = arg => (arg.includes('no-store') || arg.includes('max-age=0')) === false;
+"use strict";
 
-self.addEventListener('activate', ev => ev.waitUntil(caches.keys().then(args => Promise.all(args.filter(i => i !== name).map(i => caches.delete(i)))).catch(() => void 0)));
+const version = 1,
+	name = `ycap-v${version}`,
+	timeout = 1800,
+	urls = ["/","/assets/css/bulma.css","/assets/css/style.css","/assets/css/style.css.map","/assets/css/style.scss","/assets/css/font-awesome/css/all.css","/assets/css/font-awesome/css/all.min.css","/assets/css/font-awesome/css/brands.css","/assets/css/font-awesome/css/brands.min.css","/assets/css/font-awesome/css/fontawesome.css","/assets/css/font-awesome/css/fontawesome.min.css","/assets/css/font-awesome/css/regular.css","/assets/css/font-awesome/css/regular.min.css","/assets/css/font-awesome/css/solid.css","/assets/css/font-awesome/css/solid.min.css","/assets/css/font-awesome/css/svg-with-js.css","/assets/css/font-awesome/css/svg-with-js.min.css","/assets/css/font-awesome/css/v4-shims.css","/assets/css/font-awesome/css/v4-shims.min.css","/assets/css/font-awesome/webfonts/fa-brands-400.eot","/assets/css/font-awesome/webfonts/fa-brands-400.svg","/assets/css/font-awesome/webfonts/fa-brands-400.ttf","/assets/css/font-awesome/webfonts/fa-brands-400.woff","/assets/css/font-awesome/webfonts/fa-brands-400.woff2","/assets/css/font-awesome/webfonts/fa-regular-400.eot","/assets/css/font-awesome/webfonts/fa-regular-400.svg","/assets/css/font-awesome/webfonts/fa-regular-400.ttf","/assets/css/font-awesome/webfonts/fa-regular-400.woff","/assets/css/font-awesome/webfonts/fa-regular-400.woff2","/assets/css/font-awesome/webfonts/fa-solid-900.eot","/assets/css/font-awesome/webfonts/fa-solid-900.svg","/assets/css/font-awesome/webfonts/fa-solid-900.ttf","/assets/css/font-awesome/webfonts/fa-solid-900.woff","/assets/css/font-awesome/webfonts/fa-solid-900.woff2","/assets/img/fav_icon.png","/assets/img/icon_192.png","/assets/img/icon_512.png","/assets/img/logo.svg","/assets/js/app.js"],
+	cacheable = arg => (arg.includes("no-store") || arg.includes("max-age=0")) === false;
 
-self.addEventListener('install', ev => {
-	self.skipWaiting();
+function log (arg, type = "log", ts = true) {
+	const timestamp = type !== "error" && ts ? new Date().getTime() : 0;
 
-	return ev.waitUntil(caches.open(name).then(cache => cache.addAll(urls)).catch(() => void 0));
-});
+	if (timestamp > 0) {
+		console[type](arg instanceof Object ? arg : `[serviceworker:${timestamp}] ${arg}`);
+	} else {
+		console[type](arg);
+	}
+}
 
-self.addEventListener('fetch', ev => ev.respondWith(new Promise(async (resolve) => {
-	const method = ev.request.method;
+self.addEventListener("activate", ev => ev.waitUntil(caches.keys().then(args => {
+	const invalid = args.filter(i => i !== name);
 	let result;
 
-	if (method === 'GET') {
-		const cache = await caches.open(name),
-			cached = await cache.match(ev.request),
+	if (args.includes(name) === false) {
+		caches.open(name).then(cache => {
+			log("type=activate, cached=false, message=\"Caching core assets\"");
+
+			return cache.addAll(urls);
+		}).catch(err => log(`type=error, action=activate, message="${err.message}"`));
+	} else {
+		log("type=activate, cached=true, message=\"Reusing cached core assets\"");
+	}
+
+	if (invalid.length === 0) {
+		log("type=delete, message=\"No stale caches\"");
+		result = Promise.resolve();
+	} else {
+		log(`type=delete, message="Stale caches: ${invalid.toString()}"`);
+		result = Promise.all(invalid.map(i => {
+			log(`type=delete, message="Deleted stale cache ${i}"`);
+			caches.delete(i);
+		}));
+	}
+
+	return result;
+}).catch(() => void 0)));
+
+self.addEventListener("install", ev => {
+	self.skipWaiting();
+	ev.waitUntil(() => log("type=install, message=\"New service worker installed\""));
+});
+
+self.addEventListener("fetch", ev => ev.respondWith(new Promise(async resolve => {
+	const cache = await caches.open(name),
+		method = ev.request.method;
+	let result;
+
+	if (method === "GET") {
+		const cached = await cache.match(ev.request),
 			now = new Date().getTime();
 
 		if (cached !== void 0) {
 			const url = new URL(cached.url),
-				cdate = cached.headers.get('date'),
-				then = (cdate !== null ? new Date(cdate) : new Date()).getTime() + Number((cached.headers.get('cache-control') || '').replace(/[^\d]/g, '') || timeout) * 1e3;
+				cdate = cached.headers.get("date"),
+				then = (cdate !== null ? new Date(cdate) : new Date()).getTime() + Number((cached.headers.get("cache-control") || "").replace(/[^\d]/g, "") || timeout) * 1e3;
 
 			if (urls.includes(url.pathname) || then > now) {
 				result = cached.clone();
@@ -46,27 +70,21 @@ self.addEventListener('fetch', ev => ev.respondWith(new Promise(async (resolve) 
 
 		if (result === void 0) {
 			result = fetch(ev.request).then(res => {
-				if ((res.type === 'basic' || res.type === 'cors') && res.status === 200 && cacheable(res.headers.get('cache-control') || '')) {
+				if ((res.type === "basic" || res.type === "cors") && res.status === 200 && cacheable(res.headers.get("cache-control") || "")) {
 					cache.put(ev.request, res.clone());
 				}
 
 				return res;
-			}).catch(err => {
-				if (cached === void 0) {
-					throw err;
-				}
-
-				return cached;
-			});
+			}).catch(() => void 0);
 		}
 	} else {
 		result = fetch(ev.request).then(res => {
-			if ((res.type === 'basic' || res.type === 'cors') && res.status >= 200 && res.status < 400 && method !== 'HEAD' && method !== 'OPTIONS') {
-				cache.delete(ev.request);
+			if ((res.type === "basic" || res.type === "cors") && res.status >= 200 && res.status < 400 && method !== "HEAD" && method !== "OPTIONS") {
+				cache.delete(ev.request, {ignoreMethod: true});
 			}
 
 			return res;
-		});
+		}).catch(() => void 0);
 	}
 
 	resolve(result);
